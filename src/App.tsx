979deb6e21fd1070934a6aba5aec81e51ef5abda
Kadu <ce.magalhaespereira@gmail.com>
2022-08-31 19:39:24 -0300

    Add side bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from 'styled-components'
+import { SideBar } from './components/SideBar/SideBar'
 import { GlobalStyles } from './styles/globalStyles'
 import { defaultTheme } from './styles/themes/default'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>salut</h1>
+      <SideBar />
       <GlobalStyles />
     </ThemeProvider>
   )
---
+++ src/components/SideBar/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+import backgroundImgDesktop from '../../assets/images/bg-main-desktop.png'
+import backgroundImgMobile from '../../assets/images/bg-main-mobile.png'
+
+export const Container = styled.div`
+  background-image: url(${backgroundImgDesktop});
+  max-width: 27rem;
+  height: 100vh;
+
+  @media (max-width: ${(props) => props.theme.mobile}) {
+    background-image: url(${backgroundImgMobile});
+    max-width: 100%;
+    height: 13rem;
+  }
+`
