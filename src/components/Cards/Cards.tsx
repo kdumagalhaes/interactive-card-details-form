999e406c17a35dd2facb4492dc50943121575676
Kadu <ce.magalhaespereira@gmail.com>
2022-08-31 20:37:43 -0300

    Add front card info

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,9 +1,19 @@
 import { CardBack, CardFront, Container } from './styles'
 
+// assets
+import cardLogo from '../../assets/images/card-logo.svg'
+
 export function Cards() {
   return (
     <Container>
-      <CardFront></CardFront>
+      <CardFront>
+        <img src={cardLogo} alt="two white circles" className="card-logo" />
+        <span className="card-numbers">0000000000000000</span>
+        <div className="divider">
+          <p className="client-name">Jane Appleseed</p>
+          <span className="expiration-date">00/00</span>
+        </div>
+      </CardFront>
       <CardBack></CardBack>
     </Container>
   )
--- src/components/Cards/styles.ts
+++ src/components/Cards/styles.ts
@@ -19,11 +19,46 @@ const Card = styled.div`
   max-width: 24.84rem;
   height: 13.6rem;
   box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
+  padding: 1.5rem;
 `
 
 export const CardFront = styled(Card)`
   background-image: url(${cardFrontBackground});
   margin-bottom: 2.3rem;
+  display: flex;
+  flex-direction: column;
+
+  .card-logo {
+    max-width: 84px;
+    max-height: 47px;
+    margin-bottom: 3.8rem;
+  }
+
+  .card-numbers,
+  .client-name,
+  .expiration-date {
+    color: ${(props) => props.theme.lightGrayishViolet};
+  }
+
+  .client-name,
+  .expiration-date {
+    letter-spacing: 0.11rem;
+    font-size: 0.75rem;
+  }
+
+  .card-numbers {
+    font-size: 1.3rem;
+  }
+
+  .client-name {
+    text-transform: uppercase;
+  }
+
+  .divider {
+    display: flex;
+    justify-content: space-between;
+    margin-top: auto;
+  }
 `
 
 export const CardBack = styled(Card)`
