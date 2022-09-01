30fd191b34d543febefc4931427f8b8c8bf73453
Kadu <ce.magalhaespereira@gmail.com>
2022-09-01 19:21:35 -0300

    Add cards component

--- src/components/Cards/styles.ts
+++ src/components/Cards/styles.ts
@@ -13,12 +13,24 @@ export const Container = styled.div`
   position: absolute;
   top: 0;
   left: 180px;
+
+  @media (max-width: ${(props) => props.theme.mobile}) {
+    padding: 1.8rem 1rem 0 1rem;
+    left: 0;
+    height: auto;
+  }
 `
 
 const Card = styled.div`
   max-width: 24.84rem;
   height: 13.6rem;
   box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
+
+  @media (max-width: ${(props) => props.theme.mobile}) {
+    max-width: 15.25rem;
+    background-size: 15.25rem;
+    height: 8.35rem;
+  }
 `
 
 export const CardFront = styled(Card)`
@@ -28,26 +40,47 @@ export const CardFront = styled(Card)`
   flex-direction: column;
   padding: 1.5rem;
 
+  @media (max-width: ${(props) => props.theme.mobile}) {
+    padding: 1rem;
+    position: relative;
+    z-index: 9;
+    top: 100px;
+  }
+
   .card-logo {
     max-width: 84px;
     max-height: 47px;
     margin-bottom: 3.8rem;
+
+    @media (max-width: ${(props) => props.theme.mobile}) {
+      max-width: 46px;
+      max-height: 27px;
+      margin-bottom: 2rem;
+    }
   }
 
   .card-numbers,
   .client-name,
   .expiration-date {
     color: ${(props) => props.theme.lightGrayishViolet};
+    letter-spacing: 0.11rem;
   }
 
   .client-name,
   .expiration-date {
-    letter-spacing: 0.11rem;
     font-size: 0.75rem;
+
+    @media (max-width: ${(props) => props.theme.mobile}) {
+      font-size: 0.5rem;
+    }
   }
 
   .card-numbers {
     font-size: 1.3rem;
+
+    @media (max-width: ${(props) => props.theme.mobile}) {
+      font-size: 1rem;
+    }
   }
 
   .client-name {
@@ -68,10 +101,23 @@ export const CardBack = styled(Card)`
   align-items: center;
   justify-content: end;
 
+  @media (max-width: ${(props) => props.theme.mobile}) {
+    position: absolute;
+    width: 100%;
+    top: 30px;
+    right: 20px;
+    z-index: 0;
+  }
+
   .cvc-numbers {
     color: ${(props) => props.theme.white};
     font-size: 0.75rem;
     padding-right: 3rem;
     margin-bottom: 0.2rem;
+
+    @media (max-width: ${(props) => props.theme.mobile}) {
+      padding: 1.8rem;
+      font-size: 0.5rem;
+    }
   }
 `
